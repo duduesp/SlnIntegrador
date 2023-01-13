0a2d9697e8d29bbe54fd42b7cca7edec89e699a2
burdiaktri <burdiaktrinidad33@gmail.com>
2023-01-13 13:42:13 -0300

    Datos

---
+++ Datos/DacMedico.cs
@@ -0,0 +1,41 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public static class DacMedico
+    {
+        public static List<Medico> Select()
+        {
+            //TODO Falta implementar código -
+            return null;
+        }
+
+        public static List<Medico> Select(string especialidad)
+        {
+            //TODO Falta implementar código - Select a la tabla Medico
+            return null;
+        }
+
+        public static int Insertar(Medico medico)
+        {
+            //TODO Falta implementar código - INSERT a la tabla Medico.
+            return 0;
+        }
+        public static int Eliminar(int id)
+        {
+            //TODO Falta implementar código - DELETE a la tabla Medico.
+            return 0;
+        }
+        public static int SelectById(int id)
+        {
+            //TODO Falta implementar código - SelectById a la tabla Medico.
+            return 0;
+        }
+
+    }
+}
